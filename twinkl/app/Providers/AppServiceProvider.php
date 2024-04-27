ecd7b23e1e8f70e0817cf054c9ea22dc58e596e7
Sachith Dassanayake <sachith.net@gmail.com>
2024-04-27 23:50:44 +0100

    Setup project folder structure and generated empty classes

--- twinkl/app/Providers/AppServiceProvider.php
+++ twinkl/app/Providers/AppServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace App\Providers;
 
+use App\Repositories\SubscriptionUserRepository;
+use App\Repositories\SubscriptionUserRepositoryInterface;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -11,7 +13,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        //
+        $this->app->bind(SubscriptionUserRepositoryInterface::class, SubscriptionUserRepository::class);
     }
 
     /**
---
+++ twinkl/app/Repositories/SubscriptionUserRepository.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Repositories;
+
+use App\Repositories\SubscriptionUserRepositoryInterface;
+
+class SubscriptionUserRepository implements SubscriptionUserRepositoryInterface
+{
+
+}
